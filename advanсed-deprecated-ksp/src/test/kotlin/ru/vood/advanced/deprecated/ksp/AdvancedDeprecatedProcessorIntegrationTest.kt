fd8dd50cf14f1f0134dc0e629834e769c03de54b
vood <igor-vood@gmail.com>
2025-11-14 15:30:24 +0300

    init

--- advanсed-deprecated-ksp/src/test/kotlin/ru/vood/advanced/deprecated/ksp/AdvancedDeprecatedProcessorIntegrationTest.kt
+++ advanсed-deprecated-ksp/src/test/kotlin/ru/vood/advanced/deprecated/ksp/AdvancedDeprecatedProcessorIntegrationTest.kt
@@ -11,30 +11,30 @@ class AdvancedDeprecatedProcessorIntegrationTest {
     @TempDir
     lateinit var tempDir: File
 
-//    @Test
-//    fun `should generate errors for deprecated elements`() {
-//        // Given
-//        val testEnv = KspTestEnvironment.create()
-//        val sourceCode = """
-//            package test
-//
-//            import ru.vood.advansed.deprecated.DeprecatedWithRemoval
-//
-//            @DeprecatedWithRemoval(
-//                removalDate = "2020-01-01",
-//                deletedInVersion = "1.0.0"
-//            )
-//            class DeprecatedClass
-//        """.trimIndent()
-//
-//        // When
-//        val result = testEnv.runProcessor(
-//            processor = AdvancedDeprecatedProcessor(testEnv.environment),
-//            sources = listOf(testEnv.createSourceFile("test.kt", sourceCode)),
-//            options = mapOf("currentVersion" to "2.0.0")
-//        )
-//
-//        // Then
-//        assertTrue(result.errors.isNotEmpty(), "Should generate errors for deprecated elements")
-//    }
+   /* @Test
+    fun `should generate errors for deprecated elements`() {
+        // Given
+        val testEnv = KspTestEnvironment.create()
+        val sourceCode = """
+            package test
+
+            import ru.vood.advansed.deprecated.DeprecatedWithRemoval
+
+            @DeprecatedWithRemoval(
+                removalDate = "2020-01-01",
+                deletedInVersion = "1.0.0"
+            )
+            class DeprecatedClass
+        """.trimIndent()
+
+        // When
+        val result = testEnv.runProcessor(
+            processor = AdvancedDeprecatedProcessor(testEnv.environment),
+            sources = listOf(testEnv.createSourceFile("test.kt", sourceCode)),
+            options = mapOf("currentVersion" to "2.0.0")
+        )
+
+        // Then
+        assertTrue(result.errors.isNotEmpty(), "Should generate errors for deprecated elements")
+    }*/
 }
